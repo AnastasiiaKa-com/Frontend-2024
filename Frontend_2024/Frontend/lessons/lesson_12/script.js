c05a76d506ca94393b15562385c3552c5038bf80
AnastasiiaKa-com <nastya.karpenko.20152012@gmail.com>
2024-12-08 16:03:28 +0100

    Add files via upload

---
+++ Frontend_2024/Frontend/lessons/lesson_12/script.js
@@ -0,0 +1,29 @@
+console.log('hallo, arrays!')
+let numbers = [1, 2, 3, 4, 5]
+let fruits = ['orange', 'apple', 'banana']
+
+let iten = fruits[0]
+
+let secret = fruits[10]
+
+let arr1 = [1, 'strawberry', false, undefined]
+
+
+let length = arr1.length
+console.log('длина массива  аrr1',length)
+
+let  lastElement = arr1[arr1.length - 1]
+console.log(lastElement)
+fruits.push('pineapple');
+console.log(fruits)
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+let count = 0
+while(count < 5){
+    console.log(count)
+    
+    count++
+}
+
